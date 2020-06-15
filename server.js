6adaa7507e50cf5345b807259b52a8aab6397442
bernard love constant <top_game29@hotmail.com>
2020-06-15 19:00:14 -0400

    added code

--- server.js
+++ server.js
@@ -1,28 +1,75 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const { users } = require('./data/users');
+const { users } = require("./data/users");
 
 let currentUser = {};
+
+const findUser = (value) => {
+  return users.find((user) => Object.values(user).includes(value)) || null;
+};
+const getFriends = (arr) => {
+  return users.filter((user) => arr.includes(user._id));
+};
 
 // declare the 404 function
 const handleFourOhFour = (req, res) => {
   res.status(404).send("I couldn't find what you're looking for.");
 };
 
+const handleHomepage = (req, res) => {
+  res
+    .status(200)
+    .render("pages/homepage", { currentUser: currentUser, users: users });
+};
+
+const handleSignin = (req, res) => {
+  if (currentUser.name) {
+    res.status(307).redirect("/");
+  } else {
+    res.status(200).render("pages/signin", { currentUser: currentUser });
+  }
+};
+const handleName = (req, res) => {
+  const name = req.query.firstName;
+  const user = findeUser(name);
+
+  if (user) {
+    currentUser = user;
+    res.status(200).redirect(`users/${user._id}`);
+  } else {
+    res.status(404).redirect("/signin");
+  }
+};
+
+const handleProfilePage = (req, res) => {
+  const _id = req.params._id;
+  const user = findUser(_id);
+
+  res.status(200).render("pages/profile", {
+    user: user,
+    currentUser: currentUser,
+    friends: getFriends(user.friends),
+  });
+};
+
 // -----------------------------------------------------
 // server endpoints
 express()
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
+  .get("/", handleHomepage)
+  .get("/users/:_id", handleProfilePage)
+  .get("/signin", handleSignin)
+  .get("/getname", handleName)
 
   // a catchall endpoint that will send the 404 message.
-  .get('*', handleFourOhFour)
+  .get("*", handleFourOhFour)
 
-  .listen(8000, () => console.log('Listening on port 8000'));
+  .listen(8000, () => console.log("Listening on port 8000"));
